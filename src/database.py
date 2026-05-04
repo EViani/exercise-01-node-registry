3bd9cd1cb46c1541a72c1dcd52bf8964a183c300
EstuViani <eviani94@hotmail.com>
2026-05-04 01:05:57 -0300

    Connection manage

--- src/database.py
+++ src/database.py
@@ -7,3 +7,25 @@ Provide a dependency for FastAPI to get a DB session.
 """
 
 # TODO: Implement database connection here
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
